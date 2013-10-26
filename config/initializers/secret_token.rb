388219a7cd5d1d149eaea0126af43ea75875e961
Yujun Wu <wyj0912@gmail.com>
2013-10-26 10:47:19 -0700

    seed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Me::Application.config.secret_token = '965688d2d9c9bb273aa9027dd560568cc895bb4e5ae9b09d51545ba10868682a5de6d8a0f6f32e1de22ef689377f6d9dd20e0d1596ce85da4ec34589cb4d6b5e'
